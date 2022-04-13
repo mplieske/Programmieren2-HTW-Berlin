7f2c3f2dc6bc2fc9bd336d9497740e43ae235c28
Martin Plieske <martin.plieske@gmail.com>
2022-04-13 19:03:59 +0200

    woche-1: Methoden Teil 3 implementiert

--- MathLibraryApp/MathLibrary/Point.cs
+++ MathLibraryApp/MathLibrary/Point.cs
@@ -8,5 +8,50 @@ namespace MathLibrary
 {
     public class Point : PointVectorBase
     {
+        public static readonly Point Origin = new Point();
+
+        /// <summary>
+        /// Creates a Point instance with given x, y, z coordinates
+        /// </summary>
+        /// <param name="x">X coordinate (default = 0)</param>
+        /// <param name="y">Y coordinate (default = 0)</param>
+        /// <param name="z">Z coordinate (default = 0)</param>
+        public Point(double x = 0, double y = 0, double z = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        /// <summary>
+        /// Create a copy of sourcePoint.
+        /// </summary>
+        /// <param name="sourcePoint">The Point instance to be copied.</param>
+        public Point(Point sourcePoint)
+        {
+            X = sourcePoint.X;
+            Y = sourcePoint.Y;
+            Z = sourcePoint.Z;
+        }
+
+        /// <summary>
+        /// Calculates the distance between this Point and endPoint.
+        /// </summary>
+        /// <param name="endPoint">The Point to calculate the distance to.</param>
+        /// <returns>double representing the distance between this and endPoint.</returns>
+        public double DistanceTo(Point endPoint)
+        {
+            return CalculateDistanceTo(endPoint);
+        }
+
+        /// <summary>
+        /// Summs an array of Vectors.
+        /// </summary>
+        /// <param name="addends">The Vectors to be summed.</param>
+        /// <returns>An instance of Point where the addends sum is pointing to.</returns>
+        public Point Add(params Vector[] addends)
+        {
+            return CalculateSum(addends).AsPoint();
+        }
     }
 }
