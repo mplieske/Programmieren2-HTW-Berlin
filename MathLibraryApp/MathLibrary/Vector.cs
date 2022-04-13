ab3aebee04469f316376793c8bac95d229e70fbc
Martin Plieske <martin.plieske@gmail.com>
2022-04-13 19:26:48 +0200

    woche-1: Methoden Add und Subtract, als auch die Konstruktoren von Teil 4 implementiert

--- MathLibraryApp/MathLibrary/Vector.cs
+++ MathLibraryApp/MathLibrary/Vector.cs
@@ -8,5 +8,37 @@ namespace MathLibrary
 {
     public class Vector : PointVectorBase
     {
+        public Vector(double x = 0, double y = 0, double z = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector(Vector sourceVector)
+        {
+            X = sourceVector.X;
+            Y = sourceVector.Y;
+            Z = sourceVector.Z;
+        }
+
+        public Vector Add(params Vector[] addends)
+        {
+            return CalculateSum(addends).AsVector();
+        }
+
+        public Vector Subtract(params Vector[] subtrahends)
+        {
+            Vector firstSubtrahend = new Vector(subtrahends[0]);
+
+            for (int i = 1; i < subtrahends.Length; i++)
+            {
+                firstSubtrahend.X -= subtrahends[i].X;
+                firstSubtrahend.Y -= subtrahends[i].Y;
+                firstSubtrahend.Z -= subtrahends[i].Z;
+            }
+
+            return firstSubtrahend;
+        }
     }
 }
